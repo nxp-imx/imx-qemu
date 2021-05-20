d2b895fd1161bdbf428fde9d543c698f841529a2
Juergen Gross <jgross@suse.com>
2021-05-20 11:19:58 +0200

    xen: add pseudo RAM region for grant mappings
    Add a memory region which can be used to automatically map granted
    memory. It is starting at 0x8000000000000000ULL in order to be able to
    distinguish it from normal RAM.
    
    For this reason the xen.ram memory region is expanded, which has no
    further impact as it is used just as a container of the real RAM
    regions and now the grant region.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- include/hw/xen/xen-hvm-common.h
+++ include/hw/xen/xen-hvm-common.h
@@ -17,6 +17,7 @@
 #include <xen/hvm/ioreq.h>
 
 extern MemoryRegion ram_memory;
+extern MemoryRegion *ram_grants;
 extern MemoryListener xen_io_listener;
 extern DeviceListener xen_device_listener;
 
